a50c0493922f541b159151353343cd4daf573305
A <techbar187@gmail.com>
2018-02-16 22:39:00 +0100

    add factors for time manipulations

--- src/main/java/pl/bliw/util/Constants.java
+++ src/main/java/pl/bliw/util/Constants.java
@@ -2,4 +2,6 @@ package pl.bliw.util;
 
 public class Constants {
     public static final int ROM_CODE_OFFSET = 0x200;
+    public static final double MILI_SECONDS_FACTOR = Math.pow(10, 3);
+    public static final double NANO_SECONDS_FACTOR = Math.pow(10, 9);
 }
