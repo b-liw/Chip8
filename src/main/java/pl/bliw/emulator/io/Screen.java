737900ca4f5d4cb8d055e9aeff79644f8cbbf052
A <techbar187@gmail.com>
2018-02-18 15:03:51 +0100

    add getPixel and flipPixel in Screen module

--- src/main/java/pl/bliw/emulator/io/Screen.java
+++ src/main/java/pl/bliw/emulator/io/Screen.java
@@ -6,6 +6,16 @@ public class Screen {
     private static final int HEIGHT = 32;
     boolean[] screenState = new boolean[WIDTH * HEIGHT];
 
+    public void flipPixel(int offset) {
+        checkBounds(offset);
+        screenState[offset] = !screenState[offset];
+    }
+
+    public boolean getPixel(int offset) {
+        checkBounds(offset);
+        return screenState[offset];
+    }
+
     public static int getWidth() {
         return WIDTH;
     }
@@ -17,4 +27,11 @@ public class Screen {
     public boolean[] getScreenState() {
         return screenState;
     }
+
+    private void checkBounds(int offset) throws IllegalArgumentException {
+        if (offset < 0 || offset >= WIDTH * HEIGHT) {
+            throw new IllegalArgumentException("Invalid offset in screen buffer");
+        }
+    }
+
 }
