4775726ccc0024fc6efadd1f9e754e9f82474d67
A <techbar187@gmail.com>
2018-02-22 22:29:39 +0100

    add javadoc for Operation interface

--- src/main/java/pl/bliw/emulator/cpu/Operation.java
+++ src/main/java/pl/bliw/emulator/cpu/Operation.java
@@ -1,7 +1,12 @@
 package pl.bliw.emulator.cpu;
 
+/**
+ * Base interface for all operations which can be executed on chip8 CPU.
+ */
 public interface Operation {
-
+    /**
+     * Executes operation
+     */
     void execute();
 
 }
