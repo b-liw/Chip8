1d93011c9e6341f3eb96a2d3c3002b32beecedff
A <techbar187@gmail.com>
2018-02-22 19:58:04 +0100

    add javadoc for ErrorDialog class

--- src/main/java/pl/bliw/gui/ErrorDialog.java
+++ src/main/java/pl/bliw/gui/ErrorDialog.java
@@ -4,8 +4,15 @@ import javafx.scene.control.Alert;
 import javafx.scene.layout.Region;
 import pl.bliw.util.Constants;
 
+/**
+ * The ErrorDialog class allows to show dialog form with specified error message.
+ */
 public class ErrorDialog {
-
+    /**
+     * The method creates new error dialog and shows it to the user.
+     *
+     * @param message message to include in error dialog
+     */
     public static void show(String message) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
@@ -14,5 +21,4 @@ public class ErrorDialog {
         alert.setContentText(message);
         alert.showAndWait();
     }
-
 }
