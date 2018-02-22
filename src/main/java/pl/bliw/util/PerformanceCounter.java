c82f2955b77c16b773a6d8f47aade0855e50220e
A <techbar187@gmail.com>
2018-02-22 19:17:58 +0100

    add javadoc in PerformanceCounter class

--- src/main/java/pl/bliw/util/PerformanceCounter.java
+++ src/main/java/pl/bliw/util/PerformanceCounter.java
@@ -1,18 +1,49 @@
 package pl.bliw.util;
 
+/**
+ * The performance class counts frames per second and updates per second.
+ */
 public class PerformanceCounter {
-
+    /**
+     * The frames per second counter updated every second.
+     */
     private int FPS;
+
+    /**
+     * The updates per second counter updated every second.
+     */
     private int UPS;
+
+    /**
+     * The counter that counts the frames for one second.
+     */
     private int framesCounter;
+
+    /**
+     * The counter that counts the updates for one second.
+     */
     private int updatesCounter;
+
+    /**
+     * The counter that counts the time up to one second.
+     */
     private long nanoSecondsTime;
+
+    /**
+     * Stores previous time in nano seconds.
+     */
     private long lastTime;
 
+    /**
+     * Constructs a new PerformanceCounter instance.
+     */
     public PerformanceCounter() {
         lastTime = System.nanoTime();
     }
 
+    /**
+     * The method counts FPS and UPS and updates total counters which are available for a user.
+     */
     public void count() {
         long now = System.nanoTime();
         long delta = now - lastTime;
@@ -31,12 +62,20 @@ public class PerformanceCounter {
         }
     }
 
+    /**
+     * Returns FPS.
+     *
+     * @return frames per second
+     */
     public int getFPS() {
         return FPS;
     }
 
+    /**
+     * Returns UPS.
+     * @return updates per second
+     */
     public int getUPS() {
         return UPS;
     }
-
 }
