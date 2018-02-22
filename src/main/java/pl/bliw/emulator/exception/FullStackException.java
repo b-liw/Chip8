ceddcebf562343939e7d0f1c8f90ba0ff1cbce4d
A <techbar187@gmail.com>
2018-02-22 22:28:22 +0100

    add javadoc for FullStackException class

--- src/main/java/pl/bliw/emulator/exception/FullStackException.java
+++ src/main/java/pl/bliw/emulator/exception/FullStackException.java
@@ -1,7 +1,13 @@
 package pl.bliw.emulator.exception;
 
+/**
+ * An runtime exception for stack push operation
+ */
 public class FullStackException extends RuntimeException {
+    /**
+     * Constructs new exception
+     */
     public FullStackException() {
-        super("Cannot add new element when stack is full");
+        super("Cannot add new element if stack is full");
     }
 }
