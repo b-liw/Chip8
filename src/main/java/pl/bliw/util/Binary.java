a7f5fe64e730eb18603063ac8e855c653b70fce7
A <techbar187@gmail.com>
2018-02-22 19:55:32 +0100

    add javadoc for Binary class

--- src/main/java/pl/bliw/util/Binary.java
+++ src/main/java/pl/bliw/util/Binary.java
@@ -1,7 +1,16 @@
 package pl.bliw.util;
 
+/**
+ * The Binary class provides methods for checking if provided value is valid unsigned type.
+ */
 public class Binary {
-
+    /**
+     * The method checks if value is correct unsigned word
+     *
+     * @param message message for exception
+     * @param value   value to check
+     * @throws IllegalArgumentException if value isn't correct unsigned word.
+     */
     public static void checkIfItIsUnsignedWordOrThrow(String message, int value) throws IllegalArgumentException {
         boolean isValid = value >= 0x0 && value <= 0xFFFF;
         if (!isValid) {
@@ -9,11 +18,16 @@ public class Binary {
         }
     }
 
+    /**
+     * The method checks if value is correct unsigned byte
+     * @param message message for exception
+     * @param value value to check
+     * @throws IllegalArgumentException if value isn't correct unsigned byte.
+     */
     public static void checkIfItIsUnsignedByteOrThrow(String message, int value) throws IllegalArgumentException {
         boolean isValid = value >= 0x0 && value <= 0xFF;
         if (!isValid) {
             throw new IllegalArgumentException(String.format("Expected unsigned byte but received %d in %s%n", value, message));
         }
     }
-
 }
