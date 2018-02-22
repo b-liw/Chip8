09beb2e5e99d17c483d9013ffa1caae1a5bda254
A <techbar187@gmail.com>
2018-02-22 21:35:19 +0100

    add javadoc for Sound class

--- src/main/java/pl/bliw/emulator/io/Sound.java
+++ src/main/java/pl/bliw/emulator/io/Sound.java
@@ -5,16 +5,25 @@ import pl.bliw.util.Constants;
 
 import java.net.URL;
 
+/**
+ * The class represents sound module that can play sounds.
+ */
 public class Sound {
-
+    /**
+     * Plays sound using file from given url.
+     *
+     * @param url path to sound file.
+     */
     public void play(String url) {
         URL resource = getClass().getResource(url);
         AudioClip clip = new AudioClip(resource.toString());
         clip.play();
     }
 
+    /**
+     * Plays beep sound.
+     */
     public void beep() {
         play(Constants.BEEP_SOUND_PATH);
     }
-
 }
